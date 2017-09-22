916b1e7872d4915fb0d81bf01caf3ae2bd2c3550
neonbevz <neonbevz@gmail.com>
2017-09-22 18:47:20 +0300

    Almost ready

--- 1_Theme/src/main/java/Main.java
+++ 1_Theme/src/main/java/Main.java
@@ -6,9 +6,21 @@ import org.jsoup.select.Elements;
 import javax.print.Doc;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
+
+    private static class Review {
+        int rating;
+        String text;
+        public Review (int rating, String text) {
+            this.rating = rating;
+            this.text = text;
+        }
+    }
 
     private static void parseCategory(String url) throws IOException {
 //      Try to create "data" directory:
@@ -31,10 +43,8 @@ public class Main {
         Document html_doc = Jsoup.connect(url).get();
         Elements tiles = html_doc.select("div.g-i-tile-i-title");
         for (Element tile: tiles) {
-            String link = tile.html();
-            System.out.println(link);
-            link = tile.attr("abs:href");
-            System.out.println(link);
+            String link = tile.select("a").first().attr("href") + "comments/";
+            parseReviews(link);
         }
 
     }
@@ -43,10 +53,45 @@ public class Main {
         Document doc = Jsoup.connect(url).get();
         Elements nums = doc.select("a.paginator-catalog-l-link");
         int size = nums.size();
-        System.out.println(size);
+        if (size != 0) {
+            size = Integer.parseInt(nums.last().html());
+        }
+        List<Review> reviewsArr = new ArrayList<Review>();
+        for (int i=0; i<size; i++) {
+            String pg = url + String.format("page=%d/", i);
+            reviewsArr.addAll(parseReviewsPage(pg));
+        }
+
+        for (Review review : reviewsArr) {
+            System.out.println(String.format("%d %s", review.rating, review.text));
+        }
+
+        String filename = "data/" + url.split("/")[4] + ".csv";
+        PrintWriter writer = new PrintWriter(filename, "UTF-8");
+        for (Review review : reviewsArr) {
+            writer.println(String.format("%d;%s", review.rating, review.text));
+        }
+        writer.close();
     }
 
-    private void parseReviewsPage(String url) {
+    private static List<Review> parseReviewsPage(String url) throws IOException {
+        Document html_doc = Jsoup.connect(url).get();
+        Elements reviews = html_doc.select("article.pp-review-i");
+
+        List<Review> reviewsArr = new ArrayList<Review>();
+
+        for (Element review: reviews) {
+            Elements star = review.select("span.g-rating-stars-i");
+            Elements text = review.select("div.pp-review-text");
+            if (star.size() > 0) {
+                Elements texts = text.select("div.pp-review-text-i");
+                int star_num = Integer.parseInt(star.first().attr("content"));
+                reviewsArr.add(new Review(star_num, texts.first().html().replaceAll("<br>", " ")));
+            }
+        }
+
+        return reviewsArr;
+
 
     }
 
